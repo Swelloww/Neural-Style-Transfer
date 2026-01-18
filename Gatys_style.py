183af593ebeb91049498784ca7cae6081c446d48
Swellow <swellowwww@outlook.com>
2026-01-18 22:55:19 +0800

    删除了多余的README

--- Gatys_style.py
+++ Gatys_style.py
@@ -70,7 +70,7 @@ def tv_loss(Y_hat):
                   torch.abs(Y_hat[:, :, :, 1:] - Y_hat[:, :, :, :-1]).mean())
 
 # --- 训练逻辑 ---
-content_weight, style_weight, tv_weight = 1, 5e5, 50
+content_weight, style_weight, tv_weight = 1e-2, 5e11, 5
 
 def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
     contents_l = [content_loss(Y_hat, Y) * content_weight for Y_hat, Y in zip(contents_Y_hat, contents_Y)]
@@ -79,7 +79,7 @@ def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
     l = sum(styles_l + contents_l + [tv_l])
     return contents_l, styles_l, tv_l, l
 
-def train(content_path, style_path, num_epochs=500, lr=0.1, lr_decay_epoch=100):
+def train(content_path, style_path, num_epochs=500, lr=0.15, lr_decay_epoch=100):
     content_X = preprocess(content_path, image_shape)
     style_X = preprocess(style_path, image_shape)
     
@@ -109,16 +109,16 @@ def train(content_path, style_path, num_epochs=500, lr=0.1, lr_decay_epoch=100):
 
 # --- 执行主程序 ---
 if __name__ == "__main__":
-    content_path = './img/rainier.jpg'
-    style_path = './img/autumn-oak.jpg'
+    content_path = './img/megan.png'
+    style_path = './img/starry_night.jpg'
     
     if not os.path.exists(content_path) or not os.path.exists(style_path):
-        print("错误：请确保 ./img/ 目录下有 rainier.jpg 和 autumn-oak.jpg")
+        print("错误：请确保照片存在于 './img/' 目录下。")
     else:
         strat_time = time.time()
-        output = train(content_path, style_path, num_epochs=2000, lr=0.1)
+        output = train(content_path, style_path, num_epochs=1000, lr=0.15)
         final_img = postprocess(output)
         end_time = time.time()
         print(f"总训练时间: {end_time - strat_time:.2f} 秒")
-        final_img.save("output/Gatys_stylized_output.png")
+        final_img.save("output/Gatys_stylized_megan_output.png")
         print("训练完成！结果已保存至 output/Gatys_stylized_output.png")
